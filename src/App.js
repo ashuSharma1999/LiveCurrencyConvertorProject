ae67e059cb20b7060466cbe1c6e9cd6c5882c92a
Ashvini sharma <68182178+ashuSharma1999@users.noreply.github.com>
2025-05-27 18:52:38 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { InputBox } from "./components";
 import useCurrencyInfo from './hooks/useCurrencyInfo';
 import img from './images/currency.jpg'
